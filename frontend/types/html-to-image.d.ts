a79f9619ab3df9e4ac626c24aa1b088f4e1843f3
iparigoel <pggoel1334@gmail.com>
2025-08-17 16:04:13 +0530

    Preview feature

---
+++ frontend/types/html-to-image.d.ts
@@ -0,0 +1,16 @@
+// declare module "html-to-image" {
+//   export function toPng(
+//     node: HTMLElement,
+//     options?: Record<string, unknown>
+//   ): Promise<string>;
+
+//   export function toJpeg(
+//     node: HTMLElement,
+//     options?: Record<string, unknown>
+//   ): Promise<string>;
+
+//   export function toSvg(
+//     node: HTMLElement,
+//     options?: Record<string, unknown>
+//   ): Promise<string>;
+// }
