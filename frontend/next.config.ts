1682c10e27d90aa22ef9c1cfd6db20d8c8fe28e3
Arpit <arpitsengar99@gmail.com>
2025-09-16 01:21:36 +0530

    csp violation fix

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,27 @@ const nextConfig = {
   images: {
     domains: ['ik.imagekit.io', 'avatars.githubusercontent.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // apply to all routes
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'wasm-unsafe-eval' 'inline-speculation-rules' blob:;
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https: blob:;
+              connect-src 'self' https: wss:;
+              font-src 'self' https: data:;
+              frame-src 'self' https:;
+            `.replace(/\s{2,}/g, ' '), // compress spaces
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
