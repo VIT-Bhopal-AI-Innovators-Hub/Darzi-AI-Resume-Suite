37239eb13a8bb924be5e1f6bd9bcb20579e6401f
Arpit <arpitsengar99@gmail.com>
2025-09-16 19:34:01 +0530

    Refactor next.config.ts to comment out unused remotePatterns and headers configuration

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,40 +1,46 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "ik.imagekit.io",
-      port: "",
-      pathname: "/**",
-    },
-    {
-      protocol: "https",
-      hostname: "avatars.githubusercontent.com",
-      port: "",
-      pathname: "/**",
-    },
-  ],
-  async headers() {
-    return [
-      {
-        source: "/(.*)", // apply to all routes
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: `
-              default-src 'self';
-              script-src 'self' 'wasm-unsafe-eval' 'inline-speculation-rules' blob:;
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data: https: blob:;
-              connect-src 'self' https: wss:;
-              font-src 'self' https: data:;
-              frame-src 'self' https:;
-            `.replace(/\s{2,}/g, " "), // compress spaces
-          },
-        ],
-      },
-    ];
+  // remotePatterns: [
+  //   {
+  //     protocol: "https",
+  //     hostname: "ik.imagekit.io",
+  //     port: "",
+  //     pathname: "/**",
+  //   },
+  //   {
+  //     protocol: "https",
+  //     hostname: "avatars.githubusercontent.com",
+  //     port: "",
+  //     pathname: "/**",
+  //   },
+  // ],
+  images: {
+    domains: [
+      "avatars.githubusercontent.com",
+      "ik.imagekit.io"
+    ],
   },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)", // apply to all routes
+  //       headers: [
+  //         {
+  //           key: "Content-Security-Policy",
+  //           value: `
+  //             default-src 'self';
+  //             script-src 'self' 'wasm-unsafe-eval' 'inline-speculation-rules' blob:;
+  //             style-src 'self' 'unsafe-inline';
+  //             img-src 'self' data: https: blob:;
+  //             connect-src 'self' https: wss:;
+  //             font-src 'self' https: data:;
+  //             frame-src 'self' https:;
+  //           `.replace(/\s{2,}/g, " "), // compress spaces
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 module.exports = nextConfig;
